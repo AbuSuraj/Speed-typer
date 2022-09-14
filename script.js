71c3ea53e8e6cbdbef4e1497d40e4a43957d4155
abu bokor suraj <abusurajbd@gmail.com>
2022-09-14 22:54:02 +0600

    fixed start button count down

--- script.js
+++ script.js
@@ -105,9 +105,9 @@ const start = () => {
 
   let count = 3;
   countdownOverlay.style.display = "flex";
-
+// console.log(count)
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -134,6 +134,6 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-
+console.log(currentTime,startTime, timeSpent)
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
